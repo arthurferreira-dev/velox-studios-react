4200b20178c61fb16399bcfe310676ccd7a0b425
Arthur Ferreira <tutudacosta996@gmail.com>
2025-09-18 19:40:44 -0300

    template

---
+++ velox-studios-react/src/index.tsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './tailwind.css'
+
+createRoot(document.querySelector('body')!).render(
+  <StrictMode>
+    
+  </StrictMode>
+);
