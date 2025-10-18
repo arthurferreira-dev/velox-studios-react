ee80df11b1e3d2f41616ec21b757dc2e11486198
Arthur Ferreira <tutudacosta996@gmail.com>
2025-10-18 00:00:38 -0300

    fix buttons.tsx

--- velox-studios-react/src/utils/buttons.tsx
+++ velox-studios-react/src/utils/buttons.tsx
@@ -36,7 +36,7 @@ export function ButtonsPage(pageName: number) {
     }, []);
 
     const Pagesname: string[] = ['Jogos', 'Suporte', 'Novidades', 'KC Bot', 'Home']; //! Buttons Names
-    const Pagespath: string[] = ['https://velox-studios.vercel.app/jogos.html', 'https://velox-studios.vercel.app/suporte.html', 'https://velox-studios.vercel.app/novidades.html', 'https://velox-studios.vercel.app/kcbot.html', 'https://velox-studios.vercel.app/index.html']; //! Pages Names
+    const Pagespath: string[] = ['https://velox-studios.vercel.app/jogos.html', 'https://velox-studios.vercel.app/suporte.html', 'https://velox-studios.vercel.app/novidades.html', 'https://velox-studios.vercel.app/kcbot.html', 'https://velox-studios.vercel.app']; //! Pages Names
     
     const buttonsCL: string = "bg-slate-600 p-2 rounded-md text-lg max-[500px]:text-base text-white hover:cursor-pointer hover:bg-slate-500 duration-300"
     const iconsCL: string = click ? "ri-close-large-line hidden text-white bg-slate-600 w-[50px] text-center p-3 rounded-[100%] max-[501px]:block hover:cursor-pointer hover:bg-slate-500 duration-300 mb-4 grid-rows-subgrid" : "ri-menu-line hidden text-white bg-slate-600 w-[50px] text-center p-3 rounded-[100%] max-[762px]:block hover:cursor-pointer hover:bg-slate-500 duration-300 grid-rows-subgrid";
